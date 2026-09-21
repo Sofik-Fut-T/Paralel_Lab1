1d824a451625ccfd80cb8b09cb7a5205eff9de2e
Sofik-Fut-T <145908034+Sofik-Fut-T@users.noreply.github.com>
2026-09-21 15:23:15 +0300

    Implement multithreading to add and search in list

--- src/task1_2_3.cpp
+++ src/task1_2_3.cpp
@@ -1,1 +1,38 @@
 
+#include <iostream>
+#include <list>
+#include <thread>
+
+std::list<int> l;
+
+void AddToList(int startVal) {
+    for (int i = 0; i < 10; ++i) {
+        l.push_back(startVal + i);
+        std::cout << "[Add] Added: " << (startVal + i) << std::endl;
+    }
+}
+
+void ListContains(int searchVal) {
+    for (int i = 0; i < 10; ++i) {
+        bool found = false;
+        for (int val : l) {
+            if (val == searchVal) {
+                found = true;
+                break;
+            }
+        }
+        std::cout << "[Contains] Search " << searchVal << ": " 
+                  << (found ? "Found" : "Not found") << std::endl;
+    }
+}
+
+int main() {
+    int val = 5;
+    std::thread t1(AddToList, val);
+    std::thread t2(ListContains, val);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
