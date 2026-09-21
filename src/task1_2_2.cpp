84ae9b02a8931e3d0643288e65f08f3e81240f11
Sofik-Fut-T <145908034+Sofik-Fut-T@users.noreply.github.com>
2026-09-21 15:22:33 +0300

    Implement multithreading with two threads
    Added multithreading example with two threads printing numbers.

--- src/task1_2_2.cpp
+++ src/task1_2_2.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+#include <thread>
+
+void Thread1() {
+    std::cout << "1 ";
+}
+
+void Thread2() {
+    std::cout << "2 ";
+}
+
+int main() {
+    std::thread t1(Thread1);
+    std::thread t2(Thread2);
+
+    t1.detach();
+    t2.detach();
+
+    // Головний потік може завершитися раніше, ніж дочірні потоки виведуть дані
+    return 0;
+}
