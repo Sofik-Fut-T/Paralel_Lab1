71c0d4d17cc77190167484ea10f87b748bfe3d56
Sofik-Fut-T <145908034+Sofik-Fut-T@users.noreply.github.com>
2026-09-21 15:21:01 +0300

    Implement threads for printing numbers
    Added two threads that print numbers but lack proper handling.

--- src/task1_2_1.cpp
+++ src/task1_2_1.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+#include <thread>
+
+void Thread1() {
+    std::cout << "1 ";
+}
+
+void Thread2() {
+    std::cout << "2 ";
+}
+
+int main() {
+    // Невизначена поведінка (Undefined Behavior):
+    // Потоки знищуються без join() або detach()
+    std::thread t1(Thread1);
+    std::thread t2(Thread2);
+
+    return 0; // Додаток впаде з помилкою std::terminate
+}
